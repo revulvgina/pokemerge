62c77c7924cbfff0b2d32ae4d5c15771190e967c
ghervis <52240765+ghervis@users.noreply.github.com>
2023-07-24 06:33:30 +0800

    improve html loader

--- js/common/load-html.js
+++ js/common/load-html.js
@@ -1,34 +1,39 @@
-function loadHTML(url, hasParent) {
-	const thatCallingScript = document.currentScript;
-
-	jQuery(document).ready(function () {
-		let newElement;
-		
-		if (hasParent) {
-			newElement = thatCallingScript.parentElement;
-		}
-
-		if (!newElement) {
-			newElement = document.createElement("div");
-			newElement.id = `loaded-html-${Date.now()}-${Math.floor(
-				Math.random() * 69
-			)}`;
-			document.body.appendChild(newElement);
-		}
-
+(() => {
+	window.loadHTML = (url, hasParent) => {
+		const thatCallingScript = document.currentScript;
+	
+		jQuery(document).ready(function () {
+			let newElement;
+			
+			if (hasParent) {
+				newElement = thatCallingScript.parentElement;
+			}
+	
+			if (!newElement) {
+				newElement = document.createElement("div");
+				newElement.id = `loaded-html-${Date.now()}-${Math.floor(
+					Math.random() * 69
+				)}`;
+				document.body.appendChild(newElement);
+			}
+	
+			clearTimeout(window._setLoadedImportsTimeout);
+			window._pendingElements += 1;
+			jQuery.get(url).done((response) => {
+				jQuery(newElement).html(response);
+				window._pendingElements -= 1;
+				// console.info('attached', url);
+				0 === window._pendingElements && callLoadedImports();
+			});
+		});
+	};
+	
+	const callLoadedImports = () => {
 		clearTimeout(window._setLoadedImportsTimeout);
-    jQuery.get(url).done((response) => {
-			jQuery(newElement).html(response);
-			// console.info('attached', url);
-			callLoadedImports();
-    });
-  });
-}
+		window._setLoadedImportsTimeout = setTimeout(() => {
+			document.dispatchEvent(new CustomEvent('imports-loaded'));
+		}, 200)
+	};
 
-function callLoadedImports() {
-	clearTimeout(window._setLoadedImportsTimeout);
-	window._setLoadedImportsTimeout = setTimeout(() => {
-		document.dispatchEvent(new Event('imports-loaded'));
-		// console.info("imports-loaded");
-	}, 200)
-}
+	window._pendingElements = 0;
+})();
